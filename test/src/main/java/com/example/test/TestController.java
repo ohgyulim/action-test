5969e4a4d599f63fedaf9ce7aca356e180544872
gyulim5 <ohrbfla1004@khu.ac.kr>
2024-08-09 14:35:41 +0900

    프로젝트 파일 추가

---
+++ test/src/main/java/com/example/test/TestController.java
@@ -0,0 +1,15 @@
+package com.example.test;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@RequestMapping(method = RequestMethod.GET, value="/version")
+	public ResponseEntity<String> getVersion() {
+		return ResponseEntity.ok("Version");
+	}
+}
